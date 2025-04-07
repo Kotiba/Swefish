0f1f90888edd2d9a8ff70576a1a329c453326738
Kotiba <kotiba.marghani@gmail.com>
2025-04-07 13:20:06 +0200

    Fixing TypeScript Errors Related to isTopRated and weightOptions

---
+++ src/utils/types.ts
@@ -0,0 +1,26 @@
+// Type definitions for menu items
+
+export interface WeightOption {
+  weight: string;
+  price: number;
+}
+
+export interface MenuItem {
+  title: string;
+  description: string;
+  price: number;
+  image: string;
+  fullWidth?: boolean;
+  isTopRated?: boolean;
+  weightOptions?: WeightOption[];
+}
+
+export interface MenuCategory {
+  id: string;
+  title: string;
+  items: MenuItem[];
+}
+
+export interface MenuData {
+  menuCategories: MenuCategory[];
+} 
