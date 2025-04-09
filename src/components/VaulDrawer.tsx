82bc9c58a0323bdbec8dab4d112723762b2868c1
Kotiba <kotiba.marghani@gmail.com>
2025-04-09 22:45:41 +0200

    remove drawer footer and more rounded corners

--- src/components/VaulDrawer.tsx
+++ src/components/VaulDrawer.tsx
@@ -5,13 +5,13 @@ import { Drawer } from 'vaul';
 export default function VaulDrawer() {
   return (
     <Drawer.Root>
-      <Drawer.Trigger className="px-4 py-2 bg-black text-white text-sm rounded-full hover:bg-gray-50 transition-colors border border-gray-300 shadow-sm">
+      <Drawer.Trigger className="px-4 py-2 bg-black text-white text-sm rounded-full hover:bg-gray-800  transition-colors shadow-sm">
         Open Drawer
       </Drawer.Trigger>
       <Drawer.Portal>
         <Drawer.Overlay className="fixed inset-0 bg-black/40" />
-        <Drawer.Content className="bg-gray-100 flex flex-col rounded-t-[10px] mt-24 h-fit fixed bottom-0 left-0 right-0 outline-none">
-          <div className="p-4 bg-white rounded-t-[10px] flex-1">
+        <Drawer.Content className="bg-gray-100 flex flex-col rounded-t-[20px] mt-24 h-fit fixed bottom-0 left-0 right-0 outline-none">
+          <div className="p-4 bg-white rounded-t-[20px] flex-1">
             <div aria-hidden className="mx-auto w-12 h-1.5 flex-shrink-0 rounded-full bg-gray-300 mb-8" />
             <div className="max-w-md mx-auto">
               <Drawer.Title className="font-medium mb-4 text-gray-900">Drawer for React.</Drawer.Title>
@@ -21,13 +21,6 @@ export default function VaulDrawer() {
               <p className="text-gray-600 mb-2">
                 This is a simple drawer implementation with smooth animations and drag-to-close functionality.
               </p>
-            </div>
-          </div>
-          <div className="p-4 bg-gray-100 border-t border-gray-200 mt-auto">
-            <div className="flex gap-6 justify-end max-w-md mx-auto">
-              <Drawer.Close className="px-4 py-2 bg-yellow-600 text-white rounded-md hover:bg-yellow-700 transition-colors">
-                Close Drawer
-              </Drawer.Close>
             </div>
           </div>
         </Drawer.Content>
